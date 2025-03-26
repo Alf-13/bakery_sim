14c294a7f16c15748d3b8fdf7af381b0c65b574b
Alf-13 <alfreisinger@gmail.com>
2025-03-25 19:48:16 -0500

    Added functions for daily customers and demand

--- sim_settings.py
+++ sim_settings.py
@@ -1,7 +1,26 @@
-#Function that relates marketing spend to customer quantity
+import numpy as np
 
-#Order generator that creates the day's customers, what they are ordering, and when they want it
+#Function that relates marketing spend to customer quantity
+def daily_customers(monthly_marketing_spend):
+    if monthly_marketing_spend < 0:
+        monthly_marketing_spend = 0
+    monthly_customers = 100*monthly_marketing_spend**(1/3)+360
+    rand_monthly_customers = np.random.normal(monthly_customers, (monthly_customers*0.1))
+    return int(np.round(rand_monthly_customers/30,0))
 
 #Demand curve for bakery items
+def customer_demand(bread_price):
+    base_demand = -1*bread_price+5
+    rand_demand = int(np.round(np.random.normal(base_demand, 1),0))
+    if rand_demand > 5:
+        demand = 5
+    elif rand_demand < 1:
+        demand = 0
+    else:
+        demand = rand_demand
+    return demand  
+        
+#Order generator that creates the day's customers, what they are ordering, and when they want it
+
 
 #Expiration time
