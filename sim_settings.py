2ce21bbd4c2b433a061a58bd374a927a9e663e05
Alf-13 <alfreisinger@gmail.com>
2025-03-27 16:56:19 -0500

    Added order generator

--- sim_settings.py
+++ sim_settings.py
@@ -6,7 +6,8 @@ def daily_customers(monthly_marketing_spend):
         monthly_marketing_spend = 0
     monthly_customers = 100*monthly_marketing_spend**(1/3)+360
     rand_monthly_customers = np.random.normal(monthly_customers, (monthly_customers*0.1))
-    return int(np.round(rand_monthly_customers/30,0))
+    customers = int(np.round(rand_monthly_customers/30,0))
+    return customers
 
 #Demand curve for bakery items
 def customer_demand(bread_price):
@@ -21,6 +22,14 @@ def customer_demand(bread_price):
     return demand  
         
 #Order generator that creates the day's customers, what they are ordering, and when they want it
-
+def order_generator(monthly_marketing_spend, bread_price):
+    number_of_customers = daily_customers(monthly_marketing_spend)
+    daily_orders = np.zeros((number_of_customers,2), dtype=int)
+    for i in range(number_of_customers):
+        purchase_qty = customer_demand(bread_price)
+        purchase_time = np.random.randint(1, 600)
+        daily_orders[i] = [purchase_time, purchase_qty]
+    daily_orders = daily_orders[np.argsort(daily_orders[:,0])]
+    return daily_orders
 
 #Expiration time
